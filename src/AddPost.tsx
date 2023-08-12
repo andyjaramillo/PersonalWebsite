1469ad94f875de3d182720875e734eb5aeb6d75e
Andreas Jaramillo <jaramilloan@lancastercountryday.org>
2023-08-11 17:32:54 -0700

    Added background to blog post

--- src/AddPost.tsx
+++ src/AddPost.tsx
@@ -1,9 +1,14 @@
 import * as React from 'react';
 import axios from "axios";
 import Stack from "@mui/material/Stack";
-import {TextField} from "@mui/material";
+import {CardActions, Grid, Modal, TextField} from "@mui/material";
 import {useState} from "react";
 import Button from "@mui/material/Button";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import EmailIcon from "@mui/icons-material/Email";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
 
 
 function AddPost() {
@@ -29,17 +34,23 @@ function AddPost() {
     return (
         <div>
             <Stack spacing={2} mt={10}>
-                <TextField id="outlined-basic" label="Title" variant="outlined" value={title} onChange={ detail => setTitle(detail.target.value)}/>
-                <TextField
-                    id="outlined-multiline-static"
-                    label="Multiline"
-                    multiline
-                    rows={10}
-                    defaultValue="Add content"
-                    value={content}
-                    onChange={ detail => setContent(detail.target.value)}
-                />
-                <Button variant="contained" onClick={handleSubmit}>Submit</Button>
+                <Card sx={{ minWidth: 1000, minHeight: 100,  }}>
+                    <CardContent sx={{ backgroundColor: "#FBFAF5" , minHeight: 65}}>
+                        <TextField id="outlined-basic" label="Title" variant="outlined" value={title} onChange={ detail => setTitle(detail.target.value)}/>
+                        <TextField
+                            id="outlined-multiline-static"
+                            label="Multiline"
+                            multiline
+                            rows={10}
+                            defaultValue="Add content"
+                            value={content}
+                            onChange={ detail => setContent(detail.target.value)}
+                        />
+                    </CardContent>
+                    <CardActions sx={{ backgroundColor: "#FBFAF5"}} >
+                        <Button variant="contained" onClick={handleSubmit}>Submit</Button>
+                    </CardActions>
+                </Card>
             </Stack>
         </div>
     )
